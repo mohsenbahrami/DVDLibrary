b24a5b8c335fe662ba55b16c29b36e8292f4e739
mohsenbahrami <mohsen.bahrami60@gmail.com>
2021-10-03 10:22:05 -0400

    Viwe and Mmodel

--- src/main/java/com/mycompany/dvdlibrary/dto/DVD.java
+++ src/main/java/com/mycompany/dvdlibrary/dto/DVD.java
@@ -4,10 +4,115 @@
  */
 package com.mycompany.dvdlibrary.dto;
 
+import java.util.Objects;
+
 /**
  *
  * @author bafsharim
  */
 public class DVD {
-    
+   private String title;
+   private String releaseDate;
+   private String mpaaRating;
+   private String directorName;
+   private String studio;
+   private String rating;
+
+    public DVD() {
+    }
+
+    public DVD(String title, String releaseDate, String mpaaRating, String directorName, String studio, String rating) {
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.mpaaRating = mpaaRating;
+        this.directorName = directorName;
+        this.studio = studio;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getMpaaRating() {
+        return mpaaRating;
+    }
+
+    public void setMpaaRating(String mpaaRating) {
+        this.mpaaRating = mpaaRating;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public void setDirectorName(String directorName) {
+        this.directorName = directorName;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+    @Override
+    public int hashCode() {
+        int hash = 6;
+        hash = 97 * hash + Objects.hashCode(this.title);
+        hash = 97 * hash + Objects.hashCode(this.releaseDate);
+        hash = 97 * hash + Objects.hashCode(this.mpaaRating);
+        hash = 97 * hash + Objects.hashCode(this.directorName);
+        hash = 97 * hash + Objects.hashCode(this.studio);
+        hash = 97 * hash + Objects.hashCode(this.rating);
+        return hash;
+    }
+
+  //  @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj) {
+//            return true;
+//        }
+//        if (obj == null) {
+//            return false;
+//        }
+//        if (getClass() != obj.getClass()) {
+//            return false;
+//        }
+//        final DVD other = (DVD) obj;
+//        if (this.releaseDate != other.releaseDate) {
+//            return false;
+//        }
+//        if (!Objects.equals(this.directorName, other.directorName)) {
+//            return false;
+//        }
+//        if (!Objects.equals(this.title, other.title)) {
+//            return false;
+//        }
+//        if (!Objects.equals(this.studio, other.studio)) {
+//            return false;
+//        }
+//        return true;
+//    }
 }
--- src/main/java/com/mycompany/dvdlibrary/ui/DvdView.java
+++ src/main/java/com/mycompany/dvdlibrary/ui/DvdView.java
@@ -4,10 +4,129 @@
  */
 package com.mycompany.dvdlibrary.ui;
 
+import java.util.List;
+import com.mycompany.dvdlibrary.dto.DVD;
+
 /**
  *
  * @author bafsharim
  */
 public class DvdView {
+
+    private UserIO io;
+
+    public DvdView(UserIO io) {
+        this.io = io;
+    }
+
+    public void displayBookTrackerBanner() {
+        io.print("Welcome to the DVD Library");
+    }
+
+    public int displayMenuAndGetChoice() {
+        io.print("");
+        io.print("Main Menu");
+        io.print("1. View DVDs");
+        io.print("2. View DVD Details");
+        io.print("3. Add DVD");
+        io.print("4. Update DVD");
+        io.print("5. Delete DVD");
+        io.print("6. Exit");
+
+        return io.readInt("Please select an option:", 1, 6);
+    }
     
+     public void displayError(String error) {
+        io.print("ERROR: " + error);
+    }
+
+    public void displayAllBooks(List<DVD>dvd) {
+        io.print("");
+        io.print("All DVDs");
+        dvd.stream()
+                .forEach((d) -> io.print(d.getTitle() + " - " + d.getDirectorName()));
+    }
+    
+      public String getDvdTitle() {
+        io.print("");
+        return io.readString("Enter DVD name:");
+    }
+        public void displayBookDetails(DVD d) {        
+        io.print("");
+        io.print("Title: " + d.getTitle());
+        io.print("Release date: " + d.getReleaseDate());
+        io.print("MPAA rating: " + d.getMpaaRating());
+        io.print("Director's name: " + d.getDirectorName());
+        io.print("Studio : " +  d.getStudio());
+        io.print("User rating :" + d.getRating());
+    }
+        
+        public DVD getNewDvd() {
+        io.print("");
+        io.print("Enter new Dvd info");
+        String title = io.readString("Title");
+        String releaseDate = io.readString("Release Date");
+        String mpaaRating = io.readString("Mpaa Rating");
+        String directorNmea = io.readString("Director's name");
+        String studio = io.readString("Studio");
+        String rating = io.readString("User rating or note");
+        return new DVD(title,releaseDate,mpaaRating,directorNmea,studio,rating);
+    }
+        
+    public void displayAddSuccess() {
+        io.print("DVD added successfully");
+    }
+
+    public void displayExit() {
+        io.print("Existing DVD Tracker");
+    }
+
+    public String getDvdTitleToDelete() {
+        io.print("");
+        return io.readString("Enter DVD name to delete:");
+    }
+
+    public void displayDeleteSuccess() {
+        io.print("DVD deleted successfully");
+    }
+
+    public String getDVDTitleToUpdate() {
+        io.print("");
+        return io.readString("Enter DVD name to update:");    
+    }
+    
+     public DVD getUpdateDvd(DVD updateDvd) {
+        io.print("Updating " + updateDvd.getTitle());
+        String releaseDate = io.readString("Entre Release Date" + updateDvd.getReleaseDate() + "):");
+        String mpaaRating = io.readString(" Entre Mpaa Rating" + updateDvd.getMpaaRating() + "):");
+        String directornmae = io.readString("Entre Director's name" + updateDvd.getDirectorName() + "):");
+        String studio = io.readString("Entre Studio" + updateDvd.getStudio() + "):");
+        String rating = io.readString("Entre User rating or note" + updateDvd.getRating() + "):");
+        
+        
+       
+        if(!releaseDate.isBlank()) {
+            updateDvd.setReleaseDate(releaseDate);
+        }
+        if(!mpaaRating.isBlank()) {
+            updateDvd.setMpaaRating(mpaaRating);
+        }
+        if(!directornmae.isBlank()) {
+            updateDvd.setDirectorName(directornmae);
+        }
+        if(!studio.isBlank()){
+            updateDvd.setStudio(studio);
+        }
+        if(!rating.isBlank()){
+            updateDvd.setRating(rating);
+        }
+        return updateDvd;
+    }
+
+    public void displayUpdateSuccess() {
+        io.print("DVD updated successfully");
+    }
+
+
 }
+
